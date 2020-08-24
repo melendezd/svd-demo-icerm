603becd05cfdf6fe95d03a5cd986e69ccb6ee03f
David Melendez <davidmelendez@knights.ucf.edu>
2020-08-24 16:20:10 -0400

    Deploy

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("This is an app")
+    st.subheader("subheader")
+
+if __name__ == '__main__':
+    main()
---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+(general)\n\
+email = \"davidmelendez@knights.ucf.edu\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port=$PORT\n\
+" > ~/.stream/config.toml
