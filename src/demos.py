044843db3caeed7175de718fce989482d569d33d
David Melendez <davidmelendez@knights.ucf.edu>
2020-08-28 19:10:19 -0400

    Create working image compression demo

--- src/demos.py
+++ src/demos.py
@@ -7,12 +7,15 @@ from typing import Any, Callable, TypeVar, Generic, Type, Dict
 
 import PIL.Image  # type: ignore
 import numpy as np  # type: ignore
+import numpy.linalg  # type: ignore
 
 import streamlit as st  # type: ignore
 
 import matrix_like as matl
 
-import constants
+from constants import *
+
+import svd_tools as svdt
 
 
 class DemoName(Enum):
@@ -36,17 +39,36 @@ class SVDDemo(Generic[M], Demo):
     def __init__(self, demo_type: Type[M], examples: Dict[str, str]):
         self.demo_type = demo_type
         self.example_dict: Dict[str, M] = \
-            {label: demo_type.load(constants.RESOURCE_PATH + fname)
+            {label: demo_type.load(RESOURCE_PATH + fname)
              for label, fname in examples.items()}
 
     def display(self) -> None:
         selection = st.sidebar.selectbox(
             label='Select an example',
             options=list(self.example_dict.keys()))
-        self.example_dict[selection].display()
+
+        example = self.example_dict[selection]
+        example_rank: int = int(example.extract(lambda m: numpy.linalg.matrix_rank(m)))
+        print(example_rank)
+
+        chosen_rank = st.sidebar.slider(\
+            label="Compression Rank",
+            min_value=1,
+            max_value=example_rank,
+            value=example_rank,
+            step=1
+        )
+
+        st.subheader("Original")
+        example.display()
+
+        st.subheader("Compressed (Rank %d)" % chosen_rank)
+        example.compute(lambda mat: svdt.rank_k_approx(mat, rank=chosen_rank)).display()
 
 # Dictionary mapping image names to file paths
-image_dict = {'Raccoon': 'images/raccoon.jpg', 'Fox': 'images/fox.jpg'}
+image_dict = {'Raccoon': IMAGE_PATH + 'raccoon.jpg',
+              'Fox': IMAGE_PATH + 'fox.jpg'
+             }
 
 image_compression_demo = SVDDemo[matl.Image](matl.Image, image_dict)
 
