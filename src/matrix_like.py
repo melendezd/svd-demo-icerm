75f00ca7db1644b023cefae11e1ca039fff5e07e
David Melendez <davidmelendez@knights.ucf.edu>
2020-08-30 14:40:26 -0400

    Add audio support

--- src/matrix_like.py
+++ src/matrix_like.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from nptyping import NDArray, UInt8  # type: ignore
+from nptyping import NDArray, UInt8, Int16, Complex128  # type: ignore
 from typing import Any, Callable, TypeVar, Generic
 
 import PIL.Image  # type: ignore
 import numpy as np  # type: ignore
+
+import scipy.io.wavfile as wav  # type: ignore
+import scipy.signal as sig  # type: ignore
 
 import streamlit as st  # type: ignore
 
@@ -21,7 +24,7 @@ class IMatrixLike(Generic[T, S]):
         return fun(self.to_matrix())
 
     @staticmethod
-    def from_matrix(obj: NDArray[(Any,  Any), T], **kwargs: Any) -> S:
+    def from_matrix(mat: NDArray[(Any,  Any), T], **kwargs: Any) -> S:
         raise NotImplementedError
 
 
@@ -29,10 +32,10 @@ class IDisplayable:
     def display(self) -> None:
         raise NotImplementedError
 
-class ILoadable(Generic[T]):
+class ILoadable(Generic[S]):
     @staticmethod
     @st.cache
-    def load(path: str) -> T:
+    def load(fpath: str) -> S:
         raise NotImplementedError
 
 
@@ -42,15 +45,15 @@ class MatrixLikeDisplayableLoadable(Generic[T, S], IMatrixLike[T, S], ILoadable[
 
 
 class Image(MatrixLikeDisplayableLoadable[UInt8, 'Image']):
-    def __init__(self, ndim: UInt8, img: NDArray[(Any, Any, Any), UInt8]):
+    def __init__(self, num_channels: UInt8, img: NDArray[(Any, Any, Any), UInt8]):
         self.img = img
-        self.ndim = ndim
+        self.num_channels = num_channels
 
     def to_matrix(self) -> NDArray[(Any, Any), UInt8]:
         '''
         Flattens an image with any number of channels into a 2D matrix
         '''
-        return self.img.reshape(self.img.shape[0], self.img.shape[1] * self.ndim)
+        return self.img.reshape(self.img.shape[0], self.img.shape[1] * self.num_channels)
 
     def compute(self, fun: Callable[[NDArray], NDArray]) -> Image:
         '''
@@ -59,17 +62,17 @@ class Image(MatrixLikeDisplayableLoadable[UInt8, 'Image']):
         '''
         fun_img_mat = fun(self.to_matrix())
         fun_img_mat_normalized = np.clip(fun_img_mat, 0, 255).astype(np.uint8)
-        return Image.from_matrix(fun_img_mat_normalized, ndim=self.ndim)
+        return Image.from_matrix(fun_img_mat_normalized, num_channels=self.num_channels)
 
     @staticmethod
-    def from_matrix(obj: NDArray[(Any, Any), UInt8], **kwargs: Any) -> Image:
+    def from_matrix(mat: NDArray[(Any, Any), UInt8], **kwargs: Any) -> Image:
         '''
-        Converts a 2D array to an image given the number of channels as ndim in kwargs
+        Converts a 2D array to an image given the number of channels as num_channels in kwargs
         '''
-        ndim: UInt8 = kwargs.get('ndim')
-        return Image(ndim,
-                     obj.reshape(obj.shape[0],
-                                        obj.shape[1]//ndim, ndim))
+        num_channels: UInt8 = kwargs.get('num_channels')
+        return Image(num_channels,
+                     mat.reshape(mat.shape[0],
+                                        mat.shape[1]//num_channels, num_channels))
 
 
     def display(self):
@@ -79,7 +82,87 @@ class Image(MatrixLikeDisplayableLoadable[UInt8, 'Image']):
         st.image(self.img)
 
     @staticmethod
-    def load(path: str) -> Image:
-        img_arr = np.asarray(PIL.Image.open(path))
-        return Image(img_arr.ndim, np.asarray(PIL.Image.open(path)))
+    @st.cache
+    def load(fpath: str) -> Image:
+        img_arr = np.asarray(PIL.Image.open(fpath))
+        return Image(img_arr.shape[2], np.asarray(PIL.Image.open(fpath)))
 
+
+class Audio(MatrixLikeDisplayableLoadable[Complex128, 'Audio']):
+    def __init__(self, arr: NDArray, sample_rate: int):
+        self.audio = arr
+        self.sample_rate = sample_rate 
+        if arr.ndim == 1:
+            self.num_channels = 1
+        else:
+            self.num_channels = arr.shape[1]
+
+    def to_matrix(self) -> NDArray[(Any, Any), Complex128]:
+        freqs, times, spec = sig.stft(self.audio)
+        return spec
+
+    @staticmethod
+    def from_matrix(mat: NDArray[(Any, Any), Complex128], **kwargs: Any) -> Audio:
+        times, signal = sig.istft(mat)
+        return Audio(signal.astype(np.int16), sample_rate=kwargs['sample_rate'])
+        #return Audio(signal, sample_rate=kwargs['sample_rate'])
+
+    def compute(self, fun: Callable[[NDArray], NDArray]) -> Audio:
+        new_audio = Audio.from_matrix(fun(self.to_matrix()), sample_rate=self.sample_rate)
+
+        return new_audio
+
+
+    def display(self):
+        st.audio(self.encode_wav(self.audio, 16000))
+
+    @st.cache
+    def encode_wav(self, arr: NDArray, sample_rate: UInt8) -> bytes:
+        if arr.ndim == 1:
+            num_channels = 1
+        else:
+            raise Exception('Stereo not yet supported')
+
+        num_samples = arr.shape[0]
+        bits_per_sample = 16
+        byte_rate = int(sample_rate * num_channels * bits_per_sample / 8)
+        block_align = int(num_channels * bits_per_sample / 8)
+        data_size = int(num_samples * num_channels * bits_per_sample / 8)
+
+        raw = bytes()
+
+        # Chunk 1
+        raw += b'RIFF'
+        raw += int(data_size + 36).to_bytes(4, byteorder='big')
+        raw += b'WAVE'
+
+        # Subchunk 1
+        raw += b'fmt '
+        raw += (16).to_bytes(4, byteorder='little') # PCM
+        raw += (1).to_bytes(2, byteorder='little') # Linear quantization
+        raw += num_channels.to_bytes(2, byteorder='little')
+        raw += sample_rate.to_bytes(4, byteorder='little')
+        raw += byte_rate.to_bytes(4, byteorder='little')
+        raw += block_align.to_bytes(2, byteorder='little')
+        raw += bits_per_sample.to_bytes(2, byteorder='little')
+
+        # Subchunk 2
+        raw += b'data'
+        raw += data_size.to_bytes(4, byteorder='little')
+        #raw += b''.join((int(sample) - 1 + (1<<(bits_per_sample - 1)))\
+        #                .to_bytes(bits_per_sample//8, byteorder='little') 
+        #                for sample in arr)
+        raw += b''.join((int(sample) + (2**14))
+                        .to_bytes(bits_per_sample//8, byteorder='little') 
+                        for sample in arr)
+        #raw += b''.join(int(sample << 1 if sample >= 0 else ~(-sample)+1)
+        #                .to_bytes(bits_per_sample//8, byteorder='little') 
+        #                for sample in arr)
+        return raw
+
+
+    @staticmethod
+    @st.cache
+    def load(fpath: str) -> Audio:
+        sample_rate, signal = wav.read(fpath)
+        return Audio(signal, sample_rate)
